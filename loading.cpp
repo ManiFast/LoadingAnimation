c8b8bd69f4822ecd8cfb28c356fdf35d482619d7
MαηiFαsт <62830326+ManiFast@users.noreply.github.com>
2022-12-25 14:45:01 +0500

    Create loading.cpp

---
+++ loading.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <Windows.h>
+
+void loadnig(short duration = 80, short times = 2) // default
+{
+	for (int i = 0; i < times; i++)
+	{
+		Gotoxy(43, 10);
+		cout << "|";
+		Sleep(duration);
+		Gotoxy(43, 10);
+		cout << "/";
+		Sleep(duration);
+		Gotoxy(43, 10);
+		cout << "-";
+		Sleep(duration);
+		Gotoxy(43, 10);
+		cout << "\\";
+		Sleep(duration);
+		Gotoxy(43, 10);
+		cout << "|";
+		Sleep(duration);
+		Gotoxy(43, 10);
+	}
+}
+
+int main()
+{
+  loading();
+  
+  return 0;
+}
